fa2db86b1e21028b35b132a45502d0a75423121b
FrancoisJohannson <erhard.janak@t-online.de>
2019-09-17 08:48:48 +0200

    working with shortBuffer

--- src/Scope.kt
+++ src/Scope.kt
@@ -44,34 +44,40 @@ class Scope internal constructor(
 
 
         g.drawString("0", 0, halfheight)
-        g.drawString("Buffer-size "+buf.size, 20, 20)
+        g.drawString("Buffer-size "+shortBuffer!!.capacity(), 20, 20)
 
         val xbuf  = IntArray(this.width)
         val ybuf  = IntArray(this.width)
 
         val yFactor = halfheight/maxVol
-        //val yFactor = 0.1
-        // val xFactor = 1600/*0.1 Seconds*//this.width.toDouble()
+        var xFactor = 1600/*0.1 Seconds*//this.width.toDouble()
 
-        var xFactor = 1.0
-        if ( channels == 2)
-            xFactor = 2.0
+        //var xFactor = 1.0
+        //if ( channels == 2)
+        //     xFactor = 2.0
 
         g.drawString("xFactor $xFactor", 20, 40)
-        g.drawString("Frames "+buf.size/bytesPerFrame, 20, 60)
+        g.drawString("Frames "+shortBuffer!!.capacity()/channels, 20, 60)
         g.drawString("Channels $channels", 20, 80)
         g.drawString("yFactor $yFactor", 20, 100)
 
         println("******************************")
 
-        var refactoredValue: Int
+        var refactoredValue: Int = 0
 
         for (x in 0 until this.width) {
 
             println("Original Value: " + shortBuffer!![x])
 
             xbuf[x] = x
-            refactoredValue = ((shortBuffer!![x]*yFactor).toInt())+halfheight
+
+            if ( channels == 1)
+                refactoredValue = ((shortBuffer!![soundBufferXPos(x,xFactor)]*yFactor).toInt())+halfheight
+
+            if ( channels == 2)
+                refactoredValue = ((shortBuffer!![soundBufferXPos(x,xFactor)*2]*yFactor).toInt())+halfheight
+
+
             println("Refactored Value: " + refactoredValue)
             ybuf[x] = refactoredValue
 
@@ -86,8 +92,8 @@ class Scope internal constructor(
 
     private fun soundBufferXPos(x:Int,xFactor:Double) : Int {
         var soundBufferPos = (x*(xFactor)).toInt()
-        if ( soundBufferPos>=buf.size ) {
-            soundBufferPos %= buf.size
+        if ( soundBufferPos>=shortBuffer!!.capacity() ) {
+            soundBufferPos %= shortBuffer!!.capacity()//
         }
 
         return soundBufferPos
